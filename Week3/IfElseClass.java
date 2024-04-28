f6e18c8d0421245fcf3dcaacd8a24736a2e0f843
binnol <binnoley@gmail.com>
2024-04-28 13:38:39 +0545

    Update+

---
+++ Week3/IfElseClass.java
@@ -0,0 +1,38 @@
+package Week3;
+
+public class IfElseClass {
+    public static void main(String[] args) {
+        /*IF statement */
+        if(true){
+            System.out.println("True Statement");
+        }
+        /*IF takes boolean expression inside () */
+        int num1 = 10, num2 = 20;
+        if(num1 == num2){
+            System.out.println("Equal");
+        }
+        else{
+            System.out.println("Not Equal");
+        }
+        /*Note that else is optional */
+        /*If else if */
+        if(num1 == num2){
+            System.out.println("Equal");
+        }else if(num1 < num2){
+            System.out.println("Lesser");
+        }else{
+            System.out.println("Greater");
+        }
+        /* Note that else should be at the last */
+        /* If else is ladder */
+        if(num1 > num2){
+            System.out.println("Greater");
+        }else if(num1 !=num2){
+            System.out.println("Not Equal");
+        }else if(num1 == num2){
+            System.out.println("Equal");
+        }else{
+            System.out.println("Defailt Statement");}
+        }
+    }
+ 
